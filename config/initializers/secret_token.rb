3356151542aba195188afeff822e464c43602931
saasbook <saasbook@saasbook.(none)>
2012-04-20 12:17:12 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowSourcing::Application.config.secret_token = '699d176ed9f6f58c6200c70ba5a66345a131757cb08035d71e8fcd53da6f8476c86e1734afc0190b7cff47d598193283a82ca4803f95374ed6f3e0ef53bf096d'
